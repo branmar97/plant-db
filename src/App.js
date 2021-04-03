cbf460567d0c1e931fa2bc5001f5384b44b0b058
branmar97 <brandonmar97@gmail.com>
2021-04-03 10:35:48 -0400

    Added dynamic plant route for individual plants

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Nav from './components/Nav';
 import Footer from './components/Footer';
 import Home from './components/Home';
 import PlantsContainer from './containers/PlantsContainer';
+import Plant from './components/Plant';
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
       <div className="">
         <Nav />
         <Route exact path='/' component={Home} />
-        <Route path='/plants' component={PlantsContainer} />
+        <Route exact path='/plants' component={PlantsContainer} />
+        <Route path='/plants/:id' component={Plant} />
         <Footer />
       </div>
     </Router>
