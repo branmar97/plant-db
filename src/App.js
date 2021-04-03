aa0ea92a48462dd51874569243d6f579a9a32269
branmar97 <brandonmar97@gmail.com>
2021-04-03 10:30:04 -0400

    Configured react router links for nav component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Nav from './components/Nav';
 import Home from './components/Home';
 import PlantsContainer from './containers/PlantsContainer';
 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Nav = () => {
     return ( 
         <nav>
             <ul>
-                <h3>Plant DB</h3>
-                <li>Home</li>
-                <li>Plants</li>
+                <h2>Plant DB</h2>
+                <Link to='/'><li>Home</li></Link>
+                <Link to='/plants'><li>Plants</li></Link>
             </ul>
         </nav>
     );
