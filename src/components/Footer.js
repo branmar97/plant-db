6ae12111c2c7b0f450c1b3cbd1562dcd57fe0b18
branmar97 <brandonmar97@gmail.com>
2021-04-03 10:32:34 -0400

    Created footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return ( 
+        <div>
+            <span>Plant DB &#169; 2021 - Plant data provided by <a href='https://trefle.io/'>Trefle API</a></span>
+        </div>
+     );
+}
+ 
+export default Footer;
