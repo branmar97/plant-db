2e9bdf9440556fbdd2fa2d67839979b150d08e61
branmar97 <brandonmar97@gmail.com>
2021-04-03 10:23:58 -0400

    Created Nav and Home components

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return ( 
+        <div>
+            <h1>Plant DB</h1>
+            <p>View data on thousands of plants</p>
+        </div>
+    );
+}
+ 
+export default Home;
---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Nav = () => {
+    return ( 
+        <nav>
+            <ul>
+                <h3>Plant DB</h3>
+                <li>Home</li>
+                <li>Plants</li>
+            </ul>
+        </nav>
+    );
+}
+ 
+export default Nav;
