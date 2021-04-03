ddf1eb0d2a27255fb4f2ea39138702a74aa6802b
branmar97 <brandonmar97@gmail.com>
2021-04-03 10:27:50 -0400

    Configured react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Home from './components/Home';
+import PlantsContainer from './containers/PlantsContainer';
 
 function App() {
   return (
-    <div className="">
-      
-    </div>
+    <Router>
+      <div className="">
+        <Nav />
+        <Route exact path='/' component={Home} />
+        <Route path='/plants' component={PlantsContainer} />
+      </div>
+    </Router>
   );
 }
 
